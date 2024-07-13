e02449215c0c1ce53419526fd2d5a73a7701d925
leey00nsu <dbstndla1212@naver.com>
2024-07-13 18:06:52 +0900

    refactor(#2). 폼을 react-hook-form,zod 로 관리하기 위해 설정 추가

---
+++ src/types/user.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const SignUpSchema = z.object({
+  email: z.string().email({
+    message: '이메일 형식이 올바르지 않습니다.',
+  }),
+  nickname: z
+    .string()
+    .min(2, {
+      message: '닉네임은 2글자 이상 20글자 이하여야 합니다.',
+    })
+    .max(20, {
+      message: '닉네임은 2글자 이상 20글자 이하여야 합니다.',
+    }),
+  image: z.instanceof(File).optional(),
+});
+
+export type SignUpForm = z.infer<typeof SignUpSchema>;
