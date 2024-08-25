1be3a1c3c0ef8014a9ff1f4c91f503f728441ed8
leey00nsu <dbstndla1212@naver.com>
2024-08-25 23:51:58 +0900

    refactor(#11). useSearchParams suspense로 감싸도록 변경

--- src/app/(auth)/sign-in/callback/page.tsx
+++ src/app/(auth)/sign-in/callback/page.tsx
@@ -1,5 +1,10 @@
 import AuthCallback from '@/features/auth/ui/auth-callback';
+import { Suspense } from 'react';
 
 export default function Page() {
-  return <AuthCallback />;
+  return (
+    <Suspense>
+      <AuthCallback />;
+    </Suspense>
+  );
 }
