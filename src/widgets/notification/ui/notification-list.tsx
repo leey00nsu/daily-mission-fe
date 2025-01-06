61f20081b4decbe30e1f1adca1ad216f129e841e
leey00nsu <dbstndla1212@naver.com>
2025-01-06 22:16:11 +0900

    refactor(#11). 알림 없을때 처리

--- src/widgets/notification/ui/notification-list.tsx
+++ src/widgets/notification/ui/notification-list.tsx
@@ -6,66 +6,6 @@ import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 const NotificationList = () => {
-  // const notifications = [
-  //   {
-  //     title: '새로운 메시지가 도착했습니다',
-  //     body: '당신의 메시지함에 새로운 메시지가 있습니다.',
-  //     date: '2024-12-15',
-  //     isRead: true,
-  //   },
-  //   {
-  //     title: '업데이트가 있습니다',
-  //     body: '앱이 최신 버전으로 업데이트되었습니다.',
-  //     date: '2024-12-16',
-  //     isRead: true,
-  //   },
-  //   {
-  //     title: '친구 요청',
-  //     body: '새로운 친구 요청이 도착했습니다.',
-  //     date: '2024-12-17',
-  //   },
-  //   {
-  //     title: '이벤트 알림',
-  //     body: '곧 시작될 이벤트에 대한 알림입니다.',
-  //     date: '2024-12-18',
-  //   },
-  //   {
-  //     title: '시스템 점검',
-  //     body: '시스템 점검이 예정되어 있습니다.',
-  //     date: '2024-12-19',
-  //   },
-  //   {
-  //     title: '보안 경고',
-  //     body: '보안 경고가 발생했습니다.',
-  //     date: '2024-12-20',
-  //   },
-  //   {
-  //     title: '새로운 기능',
-  //     body: '새로운 기능이 추가되었습니다.',
-  //     date: '2024-12-21',
-  //   },
-  //   {
-  //     title: '할인 이벤트',
-  //     body: '할인 이벤트가 시작되었습니다.',
-  //     date: '2024-12-22',
-  //   },
-  //   {
-  //     title: '서비스 장애',
-  //     body: '일시적인 서비스 장애가 발생했습니다.',
-  //     date: '2024-12-23',
-  //   },
-  //   {
-  //     title: '계정 활동',
-  //     body: '계정 활동이 감지되었습니다.',
-  //     date: '2024-12-24',
-  //   },
-  //   {
-  //     title: '결제 알림',
-  //     body: '결제가 완료되었습니다.',
-  //     date: '2024-12-25',
-  //   },
-  // ];
-
   const { ref, inView } = useInView({
     threshold: 0,
   });
@@ -86,12 +26,22 @@ const NotificationList = () => {
     }
   }, [inView, isNotificationsFetching, hasNextPage]);
 
+  const notificationCount = notificationPages?.pages?.reduce(
+    (acc, page) => acc + page.data.length,
+    0,
+  );
+
   return (
     <section className="flex flex-col gap-2">
       {notificationPages?.pages?.map((page) =>
         page.data?.map((notification) => (
           <NotificationCard key={notification.id} {...notification} />
         )),
+      )}
+      {notificationCount === 0 && (
+        <div className="flex h-24 items-center justify-center">
+          알림이 없습니다.
+        </div>
       )}
 
       <div ref={ref} className="h-1" />
