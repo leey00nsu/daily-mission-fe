f43b632a7aa2cd752e022dd4c19e1ac849a103cb
leey00nsu <dbstndla1212@naver.com>
2025-01-26 19:48:39 +0900

    refactor(#11). 빌드 오류 수정

--- src/entities/user/model/mock-user.tsx
+++
@@ -1,7 +0,0 @@
-import { User } from '@/entities/user/model/type';
-
-export const MOCK_USER: User = {
-  email: 'mock@mock.com',
-  nickname: 'mock',
-  imageUrl: 'https://placehold.co/600x400?text=Mock',
-};
