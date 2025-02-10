1bfcd119955bbebedc8a050dd6a9f28cb29b53a6
leey00nsu <dbstndla1212@naver.com>
2025-02-10 21:57:17 +0900

    refactor(#11). 페이지 이동시 모달 닫기

--- src/providers/overlay-provider.tsx
+++ src/providers/overlay-provider.tsx
@@ -1,8 +1,18 @@
 'use client';
 
-import { OverlayProvider as PrimitiveOverlayProvider } from 'overlay-kit';
+import { usePathname } from 'next/navigation';
+import {
+  overlay,
+  OverlayProvider as PrimitiveOverlayProvider,
+} from 'overlay-kit';
+import { useEffect } from 'react';
 
 const OverlayProvider = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    overlay.closeAll();
+  }, [pathname]);
   return <PrimitiveOverlayProvider>{children}</PrimitiveOverlayProvider>;
 };
 
