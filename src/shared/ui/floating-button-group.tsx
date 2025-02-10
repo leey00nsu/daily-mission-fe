46b930cbb23935a67639af6e89841f72d927a947
leey00nsu <dbstndla1212@naver.com>
2025-02-10 22:12:53 +0900

    refactor(#11). 버튼 애니메이션 추가

--- src/shared/ui/floating-button-group.tsx
+++ src/shared/ui/floating-button-group.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'motion/react';
 import React from 'react';
 
 interface FloatingButtonGroupProps {
@@ -7,7 +8,12 @@ interface FloatingButtonGroupProps {
 const FloatingButtonGroup = ({ children }: FloatingButtonGroupProps) => {
   return (
     <section className="sticky bottom-0 z-30 flex w-full max-w-2xl flex-col gap-2 p-4">
-      {children}
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        {children}
+      </motion.div>
     </section>
   );
 };
