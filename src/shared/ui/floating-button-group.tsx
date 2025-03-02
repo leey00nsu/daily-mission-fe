c41bcf5ca8c4ba46772f3db42ad3d59ec6a8e8ef
leey00nsu <dbstndla1212@naver.com>
2025-03-02 19:21:12 +0900

    refactor(#11). motion depth 수정

--- src/shared/ui/floating-button-group.tsx
+++ src/shared/ui/floating-button-group.tsx
@@ -7,14 +7,13 @@ interface FloatingButtonGroupProps {
 
 const FloatingButtonGroup = ({ children }: FloatingButtonGroupProps) => {
   return (
-    <section className="sticky bottom-0 z-30 flex w-full max-w-2xl flex-col gap-2 p-4">
-      <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
-        {children}
-      </motion.div>
-    </section>
+    <motion.section
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      className="sticky bottom-0 z-30 flex w-full max-w-2xl flex-col gap-2 p-4"
+    >
+      {children}
+    </motion.section>
   );
 };
 
