e63fb059e54f82dd4bfc114d2c13a3ab954abd4a
leey00nsu <dbstndla1212@naver.com>
2025-03-02 20:05:32 +0900

    refactor(#11). 참여한 미션 없을때 처리

--- src/widgets/mission/ui/participated-mission-list.tsx
+++ src/widgets/mission/ui/participated-mission-list.tsx
@@ -6,14 +6,18 @@ import MissionListSkeleton from '@/features/mission/ui/mission-list-skeleton';
 import { formatPaginatedData } from '@/shared/lib/format';
 
 const ParticipatedMissionList = () => {
-  const { data: missions, isFetching: isMissionsFetching } =
+  const { data: missions, isLoading: isMissionsLoading } =
     useGetParticipatedMissions();
 
   return (
     <>
       <MissionList missionPages={formatPaginatedData(missions ?? [])} />
-
-      {isMissionsFetching && <MissionListSkeleton />}
+      {!isMissionsLoading && !missions?.length && (
+        <div className="flex h-40 items-center justify-center">
+          <p>참여한 미션이 없습니다.</p>
+        </div>
+      )}
+      {isMissionsLoading && <MissionListSkeleton />}
     </>
   );
 };
